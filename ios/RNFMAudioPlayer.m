3839da002bdd5a3d2f7dfd498e11cb3881de022c
Eric Lambrecht <eric@feed.fm>
2018-10-03 12:46:02 -0700

    first checkin with stubs

--- ios/RNFMAudioPlayer.m
+++ ios/RNFMAudioPlayer.m
@@ -1,5 +1,7 @@
 
 #import "RNFMAudioPlayer.h"
+#import "FeedMedia/FeedMediaCore.h"
+#import <React/RCTLog.h>
 
 @implementation RNFMAudioPlayer
 
@@ -8,6 +10,33 @@
     return dispatch_get_main_queue();
 }
 RCT_EXPORT_MODULE()
+
+RCT_EXPORT_METHOD(setToken:(NSString *)token secret:(NSString *)secret)
+{
+  [FMAudioPlayer setClientToken:token secret:secret];
+  RCTLogInfo(@"initialized! with token %@ and secret %@", token, secret);
+}
+
+RCT_EXPORT_METHOD(play)
+{
+  FMAudioPlayer *player = [FMAudioPlayer sharedPlayer];
+  RCTLogInfo(@"play called");
+  [player play];
+}
+
+RCT_EXPORT_METHOD(pause)
+{
+  FMAudioPlayer *player = [FMAudioPlayer sharedPlayer];
+  RCTLogInfo(@"pause called");
+  [player pause];
+}
+
+RCT_EXPORT_METHOD(skip)
+{
+  FMAudioPlayer *player = [FMAudioPlayer sharedPlayer];
+  RCTLogInfo(@"skip called");
+  [player skip];
+}
 
 @end
   

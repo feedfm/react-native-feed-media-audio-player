c2c8e3fc726ee8dc2d7a6c12bb1791081e3864f7
Eric Lambrecht <eric@feed.fm>
2019-01-17 12:13:48 -0800

    update to iOS lib 4.2.2

--- example/node_modules/react-native-feed-media-audio-player/ios/FeedMedia/FeedMediaCore.h
+++ example/node_modules/react-native-feed-media-audio-player/ios/FeedMedia/FeedMediaCore.h
@@ -6,7 +6,7 @@
 //  Copyright © 2017 Feed Media. All rights reserved.
 //
 
-#define FEED_MEDIA_CLIENT_VERSION @"4.2.1"
+#define FEED_MEDIA_CLIENT_VERSION @"4.2.2"
 
 // All public headers
 #import "FMAudioItem.h"

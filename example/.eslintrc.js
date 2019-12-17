ba2e3b37354943a4da1020511ed75a2c144c4aed
Arveen <arveen@feed.fm>
2019-12-17 13:20:49 +0000

    Update to RN 0.60.5, get android runnning

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -1,39 +1,4 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": [
-            "error",
-            { allow: [ "warn", "error" ] }
-        ]
-    }
+  root: true,
+  extends: '@react-native-community',
 };
